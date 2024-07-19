eb7e5a7c63c8ba7c17c67fc7becfd26c883e5292
JaydenTeoh <t3ohjingxiang@gmail.com>
2024-07-19 17:57:20 +0800

    add kwargs

--- envs/mo_mujoco/mo_mujoco_test_envs.py
+++ envs/mo_mujoco/mo_mujoco_test_envs.py
@@ -4,70 +4,70 @@ import numpy as np
 
 # ============================ Hopper ============================
 class MOHopperLight(MOHopperDR):
-    def __init__(self):
+    def __init__(self, **kwargs):
         masses = np.array([0.5, 0.5, 0.3, 0.7])
         damping = np.array([1.0, 1.0, 1.0])
         friction = np.array([1.0])
         task = np.concatenate([masses, damping, friction])
-        super().__init__(task=task)
+        super().__init__(task=task, **kwargs)
 
 class MOHopperHeavy(MOHopperDR):
-    def __init__(self):
+    def __init__(self, **kwargs):
         masses = np.array([9.0, 9.0, 8.5, 10.0])
         damping = np.array([1.0, 1.0, 1.0])
         friction = np.array([1.0])
         task = np.concatenate([masses, damping, friction])
-        super().__init__(task=task)
+        super().__init__(task=task, **kwargs)
 
 class MOHopperSlippery(MOHopperDR):
-    def __init__(self):
+    def __init__(self, **kwargs):
         masses = np.array([3.7, 4.0, 2.8, 5.3])
         damping = np.array([1.0, 1.0, 1.0])
         friction = np.array([0.1])
         task = np.concatenate([masses, damping, friction])
-        super().__init__(task=task)
+        super().__init__(task=task, **kwargs)
 
 class MOHopperLowDamping(MOHopperDR):
-    def __init__(self):
+    def __init__(self, **kwargs):
         masses = np.array([3.7, 4.0, 2.8, 5.3])
         damping = np.array([0.1, 0.1, 0.1])
         friction = np.array([1.0])
         task = np.concatenate([masses, damping, friction])
-        super().__init__(task=task)
+        super().__init__(task=task, **kwargs)
 
 class MOHopperHard(MOHopperDR):
-    def __init__(self):
+    def __init__(self, **kwargs):
         masses = np.array([0.1, 9.0, 9.0, 0.1])
         damping = np.array([0.1, 0.1, 0.1])
         friction = np.array([0.1])
         task = np.concatenate([masses, damping, friction])
-        super().__init__(task=task)
+        super().__init__(task=task, **kwargs)
 
 # ============================ Cheetah ============================
 class MOHalfCheehtahLight(MOHalfCheehtahDR):
-    def __init__(self):
+    def __init__(self, **kwargs):
         masses = np.array([0.5, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
         friction = np.array([0.4])
         task = np.concatenate([masses, friction])
-        super().__init__(task=task)
+        super().__init__(task=task, **kwargs)
 
 class MOHalfCheehtahHeavy(MOHalfCheehtahDR):
-    def __init__(self):
+    def __init__(self, **kwargs):
         masses = np.array([10.0, 9.5, 9.5, 9.5, 9.5, 9.5, 9.5])
         friction = np.array([0.4])
         task = np.concatenate([masses, friction])
-        super().__init__(task=task)
+        super().__init__(task=task, **kwargs)
 
 class MOHalfCheehtahSlippery(MOHalfCheehtahDR):
-    def __init__(self):
+    def __init__(self, **kwargs):
         masses = np.array([6.25020921, 1.54351464, 1.5874477, 1.09539749, 1.43807531, 1.20083682, 0.88451883])
         friction = np.array([0.02])
         task = np.concatenate([masses, friction])
-        super().__init__(task=task)
+        super().__init__(task=task, **kwargs)
 
 class MOHalfCheehtahHard(MOHalfCheehtahDR):
-    def __init__(self):
+    def __init__(self, **kwargs):
         masses = np.array([10.0, 0.1, 10.0, 10.0, 0.1, 0.1, 10.0])
         friction = np.array([0.02])
         task = np.concatenate([masses, friction])
-        super().__init__(task=task)
+        super().__init__(task=task, **kwargs)
