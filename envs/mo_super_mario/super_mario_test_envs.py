cced2c4455826e74bc069b2aff89e57ae91b9758
JaydenTeoh <t3ohjingxiang@gmail.com>
2024-09-27 08:46:22 +0800

    change mario zero shot

--- envs/mo_super_mario/super_mario_test_envs.py
+++ envs/mo_super_mario/super_mario_test_envs.py
@@ -77,14 +77,14 @@ def register_mario():
     stages = [
         "1-1", "1-2", "1-3", "1-4",
         "2-1", "2-2", "2-3", "2-4",
-        "3-1", "3-4",
+        "3-1", "3-2", "3-4",
         "4-1", "4-2", "4-3", "4-4",
-        "5-1", "5-3", "5-4",
+        "5-1", "5-2", "5-3", "5-4",
         "6-1", "6-2", "6-3", "6-4",
-        "7-1", "7-2", "7-4",
-        "8-2", "8-3", "8-4"
+        "7-1", "7-2", "7-3", "7-4",
+        "8-1", "8-2", "8-3", "8-4"
     ]
-    # zero-shot 7-3=2-3, 3-3=5-3=4-3, 5-3=1-3, 5-2=8-3, 8-1
+    # zero-shot 7-3=2-3, 3-3 = 3-2 + 4-3 + 5-3, 5-2=3-1=8-1
     _register_mario_env('MOSuperMarioBrosZeroShot-v0', is_random=True, rom_mode='vanilla', stages=stages)
     _register_mario_env('MOSuperMarioBrosZeroShot-v1', is_random=True, rom_mode='downsample', stages=stages)
     _register_mario_env('MOSuperMarioBrosZeroShot-v2', is_random=True, rom_mode='pixel', stages=stages)
